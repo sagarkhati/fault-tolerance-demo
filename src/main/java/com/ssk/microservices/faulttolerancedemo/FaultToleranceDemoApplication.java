b8f2b72553720358707e00538c674c312d10de77
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-14 09:46:44 +0530

    Initial Commit

---
+++ src/main/java/com/ssk/microservices/faulttolerancedemo/FaultToleranceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ssk.microservices.faulttolerancedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultToleranceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaultToleranceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssk/microservices/faulttolerancedemo/FaultToleranceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.microservices.faulttolerancedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultToleranceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
