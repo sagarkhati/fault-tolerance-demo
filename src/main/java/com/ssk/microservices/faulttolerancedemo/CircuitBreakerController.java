15735b58f073a13b6eb8d66cdcc2967f01664740
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-14 09:56:42 +0530

    Added RestTemplate in controller

--- src/main/java/com/ssk/microservices/faulttolerancedemo/CircuitBreakerController.java
+++ src/main/java/com/ssk/microservices/faulttolerancedemo/CircuitBreakerController.java
@@ -1,13 +1,19 @@
 package com.ssk.microservices.faulttolerancedemo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
 	@GetMapping("/sample-api")
 	public String sampleApi() {
-		return "Sample API";
+		
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api", String.class);
+		
+		return forEntity.getBody();
+//		return "Sample API";
 	}
 }
