e9690c84e7fd726123d1b57a5472c41987a2aaaa
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-15 22:17:14 +0530

    Added RateLimiter

--- src/main/java/com/ssk/microservices/faulttolerancedemo/CircuitBreakerController.java
+++ src/main/java/com/ssk/microservices/faulttolerancedemo/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.ssk.microservices.faulttolerancedemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -16,16 +14,17 @@ public class CircuitBreakerController {
 
 	@GetMapping("/sample-api")
 //	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name = "default")
 	public String sampleApi() {
 		
 		logger.info("sample-api called...");
 
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api",
-				String.class);
-
-		return forEntity.getBody();
-//		return "Sample API";
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api",
+//				String.class);
+//
+//		return forEntity.getBody();
+		return "Sample API";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
